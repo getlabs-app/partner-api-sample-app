f1766a7e4418e29f334dcd7375fb822bc65ff453
getlabs-sean <sean.villani@getlabs.com>
2022-03-07 17:35:04 -0500

    fix: fixing spacing to match project

--- app.js
+++ app.js
@@ -11,33 +11,33 @@ app.use(express.static('public'));
 app.use(express.json({limit: '50mb'}));
 
 const server = app.listen(port, () => {
-    console.log(`Getlabs sample application listening at http://localhost:${port}`)
+  console.log(`Getlabs sample application listening at http://localhost:${port}`)
 });
 
 // config provided by getlabs
 const getlabsConifg = {
-    hostname: 'sandbox.api.getlabs.com',
-    apiToken: '', // this is your api token provided by getlabs
-    clientId: '', // this is your getlabs provided client id
+  hostname: 'sandbox.api.getlabs.com',
+  apiToken: '', // this is your api token provided by getlabs
+  clientId: '', // this is your getlabs provided client id
 }
 
 // Simulates a logged in user. All API requests will be made for this patient.
 const loggedInUser = {
-    email: '', // email address will receive notifications
-    dob: '1970-01-01',
-    birthSex: 'female',
-    phoneNumber: '', // ex: 4805551234, phone number will receive SMS notifications
-    firstName: 'Test',
-    lastName: 'Patient'
+  email: '', // email address will receive notifications
+  dob: '1970-01-01',
+  birthSex: 'female',
+  phoneNumber: '', // ex: 4805551234, phone number will receive SMS notifications
+  firstName: 'Test',
+  lastName: 'Patient'
 }
 
 // check getlabsConifg is setup
 if (!getlabsConifg.apiToken) {
-    throw new Error('Missing API Token. Add your Getlabs API token to getlabsConifg.');
+  throw new Error('Missing API Token. Add your Getlabs API token to getlabsConifg.');
 } else if (!getlabsConifg.clientId) {
-    throw new Error('Missing clientId. Add your Getlabs client id.');
+  throw new Error('Missing clientId. Add your Getlabs client id.');
 } else if (!loggedInUser.email || !loggedInUser.phoneNumber) {
-    throw new Error('Missing user email address or phone number. Add the missing data to loggedInUser.');
+  throw new Error('Missing user email address or phone number. Add the missing data to loggedInUser.');
 }
 
 /**
@@ -45,8 +45,8 @@ if (!getlabsConifg.apiToken) {
  * @type {{}}
  */
 let oauthTokenResp = {
-    access_token: '',
-    refresh_token: ''
+  access_token: '',
+  refresh_token: ''
 }
 
 /**
@@ -54,30 +54,30 @@ let oauthTokenResp = {
  * @param bearerToken: the Authorization bearer token value for said request
  */
 const getHttpClient = (bearerToken) => superagent
-    .agent()
-    .use(superagentPrefix(`https://${getlabsConifg.hostname}`))
-    .set('accept', 'json')
-    .set('Authorization', `Bearer ${bearerToken}`);
+  .agent()
+  .use(superagentPrefix(`https://${getlabsConifg.hostname}`))
+  .set('accept', 'json')
+  .set('Authorization', `Bearer ${bearerToken}`);
 
 /**
  * this generates a signed jwt in order for us to receive an oauth access_token from getlabs
  * NOTE: we sign the jwt with the apiToken as your secret
  */
 const generateJwtForOauthToken = () => {
-    const jwt = require('jsonwebtoken');
+  const jwt = require('jsonwebtoken');
 
-    // build header
-    const jwtHeader = {
-        algorithm: 'HS512',
-    };
+  // build header
+  const jwtHeader = {
+    algorithm: 'HS512',
+  };
 
-    // build payload
-    const jwtPayload = {
-        sub: getlabsConifg.clientId,
-        aud: `https://${getlabsConifg.hostname}/oauth/token`,
-    };
+  // build payload
+  const jwtPayload = {
+    sub: getlabsConifg.clientId,
+    aud: `https://${getlabsConifg.hostname}/oauth/token`,
+  };
 
-    return jwt.sign(jwtPayload, getlabsConifg.apiToken, jwtHeader);
+  return jwt.sign(jwtPayload, getlabsConifg.apiToken, jwtHeader);
 };
 
 /**
@@ -85,16 +85,16 @@ const generateJwtForOauthToken = () => {
  * @type {Promise<unknown>}
  */
 function setOauthTokenResp() {
-    return getHttpClient(generateJwtForOauthToken())
-        .post('/oauth/token')
-        .then(async (response) => {
-            console.log(`Token response received: storing token response`);
-            oauthTokenResp = response.body;
-        })
-        .catch((error) => {
-            console.error('Error fetching access token from Getlabs:', util.inspect(error.response?.body, false, null, true));
-            server.close();
-        });
+  return getHttpClient(generateJwtForOauthToken())
+    .post('/oauth/token')
+    .then(async (response) => {
+      console.log(`Token response received: storing token response`);
+      oauthTokenResp = response.body;
+    })
+    .catch((error) => {
+      console.error('Error fetching access token from Getlabs:', util.inspect(error.response?.body, false, null, true));
+      server.close();
+    });
 };
 
 /**
@@ -102,22 +102,22 @@ function setOauthTokenResp() {
  * @param refresh_token
  */
 function refreshAccessToken(access_token, refresh_token) {
-    return getHttpClient(access_token)
-        .post(`/oauth/refresh_token`)
-        .query({ refresh_token: refresh_token})
-        .then((response) => {
-            console.log(`Refreshing tokens: success`);
-            oauthTokenResp = response.body;
-        })
-        .catch(async (error) => {
-            // if we get a 400 back then our refresh token is expired and we need to get a new one
-            if (error.status == 400) {
-                console.log('Refreshing tokens: failed')
-                await setOauthTokenResp();
-            } else {
-                console.error('Error fetching access token from Getlabs:', util.inspect(error.response?.body, false, null, true));
-            }
-        });
+  return getHttpClient(access_token)
+    .post(`/oauth/refresh_token`)
+    .query({refresh_token: refresh_token})
+    .then((response) => {
+      console.log(`Refreshing tokens: success`);
+      oauthTokenResp = response.body;
+    })
+    .catch(async (error) => {
+      // if we get a 400 back then our refresh token is expired and we need to get a new one
+      if (error.status == 400) {
+        console.log('Refreshing tokens: failed')
+        await setOauthTokenResp();
+      } else {
+        console.error('Error fetching access token from Getlabs:', util.inspect(error.response?.body, false, null, true));
+      }
+    });
 
 }
 
@@ -126,43 +126,43 @@ function refreshAccessToken(access_token, refresh_token) {
  * Stores the resulting promise so the the patient's ID can be added to requests.
  */
 function getGetlabsPatient(access_token) {
-    getHttpClient(access_token).post('/patient')
-        .send(loggedInUser)
-        .then((response) => {
-            console.log(`Patient fetched from https://${getlabsConifg.hostname}/patient, all API requests will be made for this patient:`, response.body);
-            return response.body;
-        })
-        .catch((error) => {
-            console.error('Error fetching patient from Getlabs:', util.inspect(error.response?.body, false, null, true));
-        });
+  getHttpClient(access_token).post('/patient')
+    .send(loggedInUser)
+    .then((response) => {
+      console.log(`Patient fetched from https://${getlabsConifg.hostname}/patient, all API requests will be made for this patient:`, response.body);
+      return response.body;
+    })
+    .catch((error) => {
+      console.error('Error fetching patient from Getlabs:', util.inspect(error.response?.body, false, null, true));
+    });
 }
 
 /**
  * Proxies the request to the Getlabs API and sends the response.
  */
 const getlabsApiProxy = (req, res) => {
-    const logRequest = (status) => console.log(`[Getlabs request] ${req.method} ${status} https://${getlabsConifg.hostname}${req.originalUrl}`);
-    const glReq = getHttpClient(oauthTokenResp.access_token)[req.method.toLowerCase()](req.originalUrl);
-    if (!['GET', 'DELETE'].includes(req.method)) {
-        glReq.send(req.body);
-    }
-    glReq.then((response) => {
-        logRequest(response.status);
-        res.statusCode = response.status;
-        res.send(response.body);
-    })
+  const logRequest = (status) => console.log(`[Getlabs request] ${req.method} ${status} https://${getlabsConifg.hostname}${req.originalUrl}`);
+  const glReq = getHttpClient(oauthTokenResp.access_token)[req.method.toLowerCase()](req.originalUrl);
+  if (!['GET', 'DELETE'].includes(req.method)) {
+    glReq.send(req.body);
+  }
+  glReq.then((response) => {
+    logRequest(response.status);
+    res.statusCode = response.status;
+    res.send(response.body);
+  })
     .catch(async (error) => {
-        if (error.status == 401) {
-            // refresh tokens
-            await refreshAccessToken(oauthTokenResp.access_token, oauthTokenResp.refresh_token);
+      if (error.status == 401) {
+        // refresh tokens
+        await refreshAccessToken(oauthTokenResp.access_token, oauthTokenResp.refresh_token);
 
-            //retry request with refreshed tokens
-            getlabsApiProxy(req, res)
-        } else {
-            logRequest(error.status);
-            res.statusCode = error.status;
-            res.send(error.response.body);
-        }
+        //retry request with refreshed tokens
+        getlabsApiProxy(req, res)
+      } else {
+        logRequest(error.status);
+        res.statusCode = error.status;
+        res.send(error.response.body);
+      }
     });
 }
 
@@ -180,9 +180,9 @@ app.get('/availability', getlabsApiProxy);
 app.post('/file', getlabsApiProxy);
 
 app.post(['/payment/setup', '/appointment'], (req, res) => {
-    // only allow requests by the logged in patient
-    getGetlabsPatient(oauthTokenResp.access_token).then((patient) => {
-        req.body.patientId = patient.id;
-        getlabsApiProxy(req, res);
-    });
+  // only allow requests by the logged in patient
+  getGetlabsPatient(oauthTokenResp.access_token).then((patient) => {
+    req.body.patientId = patient.id;
+    getlabsApiProxy(req, res);
+  });
 });
