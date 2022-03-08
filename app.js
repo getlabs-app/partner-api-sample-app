ec5cea46cc66e36698c176a80e2bcba9d13a8aa1
getlabs-sean <sean.villani@getlabs.com>
2022-03-08 11:33:03 -0500

    fix: creating auth.js for readability

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const util = require('util');
-const superagent = require('superagent');
-const superagentPrefix = require('superagent-prefix');
-const jwt = require("jsonwebtoken");
+const Auth = require("./auth");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -41,91 +39,19 @@ if (!getlabsConifg.apiToken) {
 }
 
 /**
- * this houses the oauth token response from getlabs api
- * @type {{}}
+ * Authenticate to getlabs api
  */
-let oauthTokenResp = {
-  access_token: '',
-  refresh_token: ''
-}
-
-/**
- * returns the http client used to make external requests
- * @param bearerToken: the Authorization bearer token value for said request
- */
-const getHttpClient = (bearerToken) => superagent
-  .agent()
-  .use(superagentPrefix(`https://${getlabsConifg.hostname}`))
-  .set('accept', 'json')
-  .set('Authorization', `Bearer ${bearerToken}`);
-
-/**
- * this generates a signed jwt in order for us to receive an oauth access_token from getlabs
- * NOTE: we sign the jwt with the apiToken as your secret
- */
-const generateJwtForOauthToken = () => {
-
-  // build header
-  const jwtHeader = {
-    algorithm: 'HS512',
-  };
-
-  // build payload
-  const jwtPayload = {
-    sub: getlabsConifg.clientId,
-    aud: `https://${getlabsConifg.hostname}/oauth/token`,
-  };
-
-  return jwt.sign(jwtPayload, getlabsConifg.apiToken, jwtHeader);
-};
-
-/**
- * this method fetches an access token from getlabs api in order to make requests
- * @type {Promise<unknown>}
- */
-function setOauthTokenResp() {
-  return getHttpClient(generateJwtForOauthToken())
-    .post('/oauth/token')
-    .then(async (response) => {
-      console.log(`Token response received: storing token response`);
-      oauthTokenResp = response.body;
-    })
-    .catch((error) => {
-      console.error('Error fetching access token from Getlabs:', util.inspect(error.response?.body, false, null, true));
-      server.close();
-    });
-};
-
-/**
- * this method is used to update your access token in the event that it expires
- * @param refresh_token
- */
-function refreshAccessToken(access_token, refresh_token) {
-  return getHttpClient(access_token)
-    .post(`/oauth/refresh-token`)
-    .query({refresh_token: refresh_token})
-    .then((response) => {
-      console.log(`Refreshing tokens: success`);
-      oauthTokenResp = response.body;
-    })
-    .catch(async (error) => {
-      // if we get a 400 back then our refresh token is expired and we need to get a new one
-      if (error.status == 400) {
-        console.log('Refreshing tokens: failed')
-        await setOauthTokenResp();
-      } else {
-        console.error('Error fetching access token from Getlabs:', util.inspect(error.response?.body, false, null, true));
-      }
-    });
-
-}
+const auth = new Auth(getlabsConifg);
+auth.authenticate().then(()=>{
+  getGetlabsPatient(auth.getAuthTokens().access_token);
+});
 
 /**
  * Function to Fetch or create the patient during startup based on the loggedInUser set above.
  * Stores the resulting promise so the the patient's ID can be added to requests.
  */
 function getGetlabsPatient(access_token) {
-  return getHttpClient(access_token).post('/patient')
+  return auth.getHttpClient(access_token).post('/patient')
     .send(loggedInUser)
     .then((response) => {
       console.log(`Patient fetched from https://${getlabsConifg.hostname}/patient, all API requests will be made for this patient:`, response.body);
@@ -141,7 +67,7 @@ function getGetlabsPatient(access_token) {
  */
 const getlabsApiProxy = (req, res) => {
   const logRequest = (status) => console.log(`[Getlabs request] ${req.method} ${status} https://${getlabsConifg.hostname}${req.originalUrl}`);
-  const glReq = getHttpClient(oauthTokenResp.access_token)[req.method.toLowerCase()](req.originalUrl);
+  const glReq = auth.getHttpClient(auth.getAuthTokens().access_token)[req.method.toLowerCase()](req.originalUrl);
   if (!['GET', 'DELETE'].includes(req.method)) {
     glReq.send(req.body);
   }
@@ -153,7 +79,7 @@ const getlabsApiProxy = (req, res) => {
     .catch(async (error) => {
       if (error.status == 401) {
         // refresh tokens
-        await refreshAccessToken(oauthTokenResp.access_token, oauthTokenResp.refresh_token);
+        await auth.refreshAccessToken(auth.getAuthTokens().access_token, auth.getAuthTokens().refresh_token);
 
         //retry request with refreshed tokens
         getlabsApiProxy(req, res)
@@ -166,15 +92,6 @@ const getlabsApiProxy = (req, res) => {
 }
 
 /**
- * kick off the service
- */
-setOauthTokenResp().then(()=>{
-  getGetlabsPatient(oauthTokenResp.access_token);
-});
-
-
-
-/**
  * setup service endpoints
  */
 
@@ -184,7 +101,7 @@ app.post('/file', getlabsApiProxy);
 
 app.post(['/payment/setup', '/appointment'], (req, res) => {
   // only allow requests by the logged in patient
-  getGetlabsPatient(oauthTokenResp.access_token).then((patient) => {
+  getGetlabsPatient(auth.getAuthTokens().access_token).then((patient) => {
     req.body.patientId = patient.id;
     getlabsApiProxy(req, res);
   });
---
+++ auth.js
@@ -0,0 +1,100 @@
+const superagent = require("superagent");
+const superagentPrefix = require("superagent-prefix");
+const jwt = require("jsonwebtoken");
+const util = require("util");
+
+/**
+ * Auth class is used to make calls to getlabs api
+ */
+class Auth {
+
+  constructor(getlabsConifg) {
+    this.getlabsConifg = getlabsConifg;
+    this.oauthTokenResp = {
+      access_token: '',
+      refresh_token: ''
+    }
+  }
+
+  /**
+   * returns populated auth tokens
+   * @returns {*|{access_token: string, refresh_token: string}}
+   */
+  getAuthTokens() {
+    return this.oauthTokenResp;
+  }
+
+  /**
+   * returns the http client used to make external requests
+   * @param bearerToken: the Authorization bearer token value for said request
+   */
+  getHttpClient(bearerToken){
+   return superagent
+     .agent()
+     .use(superagentPrefix(`https://${this.getlabsConifg.hostname}`))
+     .set('accept', 'json')
+     .set('Authorization', `Bearer ${bearerToken}`);
+  }
+
+  /**
+   * generates a signed jwt specific to the partner
+   * @returns {*}
+   */
+  generateJwtForOauthToken(){
+    // build header
+    const jwtHeader = {
+      algorithm: 'HS512',
+    };
+
+    // build payload
+    const jwtPayload = {
+      sub: this.getlabsConifg.clientId,
+      aud: `https://${this.getlabsConifg.hostname}/oauth/token`,
+    };
+
+    return jwt.sign(jwtPayload, this.getlabsConifg.apiToken, jwtHeader);
+  };
+
+  /**
+   * kicks off the authentication flow w getlabs
+   * @returns {Promise<unknown>}
+   */
+  authenticate() {
+    return this.getHttpClient(this.generateJwtForOauthToken())
+      .post('/oauth/token')
+      .then(async (response) => {
+        console.log(`Token response received: storing token response`);
+        this.oauthTokenResp = response.body;
+      })
+      .catch((error) => {
+        console.error('Error fetching access token from Getlabs:', util.inspect(error.response?.body, false, null, true));
+        server.close();
+      });
+  };
+
+  /**
+   * update your access token in the event that it expires
+   * @param refresh_token
+   */
+  refreshAccessToken(access_token, refresh_token) {
+    return this.getHttpClient(access_token)
+      .post(`/oauth/refresh-token`)
+      .query({refresh_token: refresh_token})
+      .then((response) => {
+        console.log(`Refreshing tokens: success`);
+        this.oauthTokenResp = response.body;
+      })
+      .catch(async (error) => {
+        // if we get a 400 back then our refresh token is expired and we need to get a new one
+        if (error.status == 400) {
+          console.log('Refreshing tokens: failed')
+          await this.authenticate();
+        } else {
+          console.error('Error fetching access token from Getlabs:', util.inspect(error.response?.body, false, null, true));
+        }
+      });
+  }
+
+}
+
+module.exports = Auth
